f4396b4c4c96dcb2aa09ae5c43f9fa0822c79eb8
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-03 16:09:45 +0530

    updated

---
+++ secret.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class secret
+{
+    public static void main(String args[])
+    {
+        //The computer ignores this
+        Scanner sc=new Scanner(System.in);
+        System.out.println("The computer sees this");
+
+    }
+}
---
+++ the_welcome_sign.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class the_welcome_sign
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to Java");
+        System.out.println("My name is Anant Tiwari");
+    }
+} 
