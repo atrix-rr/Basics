a8123ab2bd8d046e9cdc1fa2127bb60ec8841e1c
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-09 23:28:40 +0530

    updated

---
+++ TheMath.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class TheMath
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println();
+        int x=20;
+        System.out.println();
+        int y=10;
+
+        int S=x+y;
+
+        System.out.println("Sum is:"+S);
+
+        int P=x*y;
+
+        System.out.println("The Product is:"+P);
+    }
+}
