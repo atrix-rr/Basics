ed0d282ba269a9e183f131177747b62b1d597b3c
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-09 23:20:28 +0530

    updated

---
+++ ID.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class ID
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your Name");
+        String N=sc.nextLine();
+
+        System.out.println("Enter Age");
+        int A=sc.nextInt();
+
+        System.out.println("My name is "+N+ " and age is "+A);
+    }
+}
