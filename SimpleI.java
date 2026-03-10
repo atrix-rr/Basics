a98ae92b248e2e34f9448d21e93956e7e2aeaa18
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-10 21:03:05 +0530

    updated

---
+++ SimpleI.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class SimpleI
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Principle Amount");
+        double P=sc.nextDouble();
+
+        System.out.println("Enter the Full term");
+        int T=sc.nextInt();
+
+        System.out.println("Enter Rate of Interest");
+        double R=sc.nextDouble();
+
+        double SI=(P*R*T)/100;
+
+        System.out.println("The SI is:"+SI);
+    }
+}
