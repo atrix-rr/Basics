1b3d9586616a44d390d4c502a4cfaaf2df0aa8a4
Anant Tiwari <ananttiwari231@gmail.com>
2026-03-14 23:23:59 +0530

    updated

---
+++ Add.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Add
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter 2 numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        int Sum=a+b;
+
+        System.out.println("The Sum is:"+Sum);
+    }
+}
